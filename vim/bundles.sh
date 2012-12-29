b5977305471e80144474bd15ccd27f2d2512de52
Valentin Kuznetsov <vkuznet@gmail.com>
2012-12-29 11:09:52 -0500

    Add new script to get all bundles I use in vim

---
+++ vim/bundles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mkdir -p bundle
+cd bundle
+git clone git://github.com/mileszs/ack.vim.git
+git clone git://github.com/wincent/Command-T.git
+git clone git://github.com/kien/ctrlp.vim.git
+git clone git://github.com/sjl/gundo.vim.git
+git clone git://github.com/scrooloose/nerdcommenter.git
+git clone git://github.com/scrooloose/nerdtree.git
+git clone git://github.com/honza/snipmate-snippets.git
+git clone git://github.com/ervandew/supertab.git
+git clone git://github.com/scrooloose/syntastic.git
+git clone git://github.com/majutsushi/tagbar.git
+git clone git://github.com/tomtom/tlib_vim.git
+git clone git://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone git://github.com/skammer/vim-css-color.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/Lokaltog/vim-powerline.git
+git clone git://github.com/garbas/vim-snipmate.git
+git clone git://github.com/tpope/vim-surround.git
+git clone git://github.com/sjl/learnvimscriptthehardway.git
